df42484c13fbc65b37e568802784d7b173bbc779
Dilum Aluthge <dilum@aluthge.com>
2020-01-14 06:25:24 -0500

    Update repo_url.jl

--- src/repo_url.jl
+++ src/repo_url.jl
@@ -1,6 +1,6 @@
 function _get_repo_url_with_auth(org::AbstractString, pkg::AbstractString; token::Union{AbstractString, Nothing})
     if token === nothing
-        return "https://@github.com/$(org)/$(pkg).jl"
+        return "https://github.com/$(org)/$(pkg).jl"
     else
         return "https://x-access-token:$(token)@github.com/$(org)/$(pkg).jl"
     end
