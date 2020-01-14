9167085b08d1b484e686db5e073e8f8c884054da
Dilum Aluthge <dilum@aluthge.com>
2020-01-14 06:25:44 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,15 @@ using MassInstallAction
 using Test
 
 @testset "MassInstallAction.jl" begin
+    @testset "public.jl" begin
+        for workflow in [MassInstallAction.compat_helper(),
+                         MassInstallAction.version_vigilante()]
+            @test MassInstallAction.install(workflow, "bcbi"; token = nothing, cc = ["@bcbi"]) == nothing
+        end
+    end
+
+    @testset "repo_url.jl" begin
+        @test MassInstallAction._get_repo_url_with_auth("MYORG", "MYPKG"; token = "MYTOKEN") == "https://x-access-token:MYTOKEN@github.com/MYORG/MYPKG.jl"
+        @test MassInstallAction._get_repo_url_with_auth("MYORG", "MYPKG"; token = nothing) == "https://github.com/MYORG/MYPKG.jl"
+    end
 end
