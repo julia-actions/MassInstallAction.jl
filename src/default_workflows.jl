4d8374fa165c2512144449181c17a65fe04eb659
Dilum Aluthge <dilum@aluthge.com>
2021-04-11 09:44:06 -0400

    Fix the URL for the CompatHelper workflow file (#36)

--- src/default_workflows.jl
+++ src/default_workflows.jl
@@ -3,7 +3,7 @@ import HTTP
 function compat_helper()
     name = "CompatHelper"
     files_to_create = Dict{String, String}()
-    files_to_create["CompatHelper.yml"] = String(HTTP.get("https://raw.githubusercontent.com/bcbi/CompatHelper.jl/master/.github/workflows/CompatHelper.yml").body)
+    files_to_create["CompatHelper.yml"] = String(HTTP.get("https://raw.githubusercontent.com/JuliaRegistries/CompatHelper.jl/master/.github/workflows/CompatHelper.yml").body)
     files_to_delete = Set{String}()
     return Workflow(name, files_to_create, files_to_delete)
 end
